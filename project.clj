34ad51535b09fa6f8f53869f23f50fbdb2a5ba38
vemv <vemv@users.noreply.github.com>
2021-12-28 17:37:59 +0100

    Start using `mranderson`
    Solves possible conflicts with lein-doo and other plugins.

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject mx.cider/enrich-classpath "1.5.2"
+(defproject mx.cider/enrich-classpath "1.6.0"
   :description "Makes available .jars with Java sources and javadocs for a given project."
 
   :url "https://github.com/clojure-emacs/enrich-classpath"
@@ -7,12 +7,15 @@
             :url  "https://www.eclipse.org/legal/epl-2.0/"}
 
   :dependencies [[org.clojure/clojure "1.10.3"] ;; Hard-require a recent-enough version of Clojure, since other plugins may require an overly old one which would make Fipp fail.
-                 [fipp "0.6.24" :exclusions [org.clojure/clojure
-                                             org.clojure/core.rrb-vector]]
-                 ;; https://github.com/brandonbloom/fipp/pull/79
-                 [org.clojure/core.rrb-vector "0.1.2"]]
+                 ^:inline-dep [fipp "0.6.25" :exclusions [org.clojure/clojure]]]
 
   :eval-in-leiningen ~(nil? (System/getenv "no_eval_in_leiningen"))
+
+  :plugins [[thomasa/mranderson "0.5.4-SNAPSHOT"]]
+
+  :mranderson {:project-prefix  "cider.enrich-classpath.inlined-deps"
+               :expositions     []
+               :unresolved-tree false}
 
   :deploy-repositories [["clojars" {:url "https://clojars.org/repo"
                                     :username :env/clojars_username
