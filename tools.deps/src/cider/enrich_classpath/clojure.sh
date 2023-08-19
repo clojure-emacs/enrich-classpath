8ea25047fd93543e0df13d208fb1fb92d1e8f851
vemv <vemv@users.noreply.github.com>
2023-08-19 06:36:20 +0200

    `clojure.sh`: use a temp dir

--- tools.deps/src/cider/enrich_classpath/clojure.sh
+++ tools.deps/src/cider/enrich_classpath/clojure.sh
@@ -20,10 +20,10 @@ elif [[ "$*" == *Scp* ]]; then
 else
 
   here="$PWD"
+  there=$(mktemp -d -t mytempdir.XXXXXX)
 
   # don't let local deps.edn files interfere:
-  # XXX prefer a tmp dir. Make x-platform
-  cd
+  cd "$there"
 
   # enrich-classpath will emit a command starting by "clojure", or print a stacktrace:
   output=$(2>&1 "$clojure" -Sforce -Srepro -J-XX:-OmitStackTraceInFastThrow -J-Dclojure.main.report=stderr -Sdeps '{:deps {mx.cider/tools.deps.enrich-classpath {:mvn/version "1.13.0"}}}' -M -m cider.enrich-classpath.clojure "$clojure" "$here" "true" "$@")
