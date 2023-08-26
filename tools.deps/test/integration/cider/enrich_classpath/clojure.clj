9d6404242dabc8f762d950c054a337c0e1c9af8e
vemv <vemv@users.noreply.github.com>
2023-08-26 21:42:14 +0200

    x

--- tools.deps/test/integration/cider/enrich_classpath/clojure.clj
+++ tools.deps/test/integration/cider/enrich_classpath/clojure.clj
@@ -3,6 +3,7 @@
    [cider.enrich-classpath.clojure :as sut]
    [cider.enrich-classpath.jdk :as jdk]
    [clojure.java.io :as io]
+   [clojure.string :as string]
    [clojure.test :refer [deftest is testing]]))
 
 (deftest works
@@ -45,16 +46,34 @@
           (when (re-find #"^1\.8\." (System/getProperty "java.version"))
             (is (-> actual (.contains "unzipped-jdk-sources"))))))))
 
-  (is (any? (println (sut/impl "clojure"
-                               "deps.edn"
-                               (str (io/file (System/getProperty "user.dir") "test-resources" "extra-project"))
-                               []
-                               false)))
+  (is (any? (sut/impl "clojure"
+                      "deps.edn"
+                      (str (io/file (System/getProperty "user.dir") "test-resources" "extra-project"))
+                      []
+                      false))
       "Can run a once-problematic project")
 
-  (is (any? (println (sut/impl "clojure"
-                               "deps.edn"
-                               (str (io/file (System/getProperty "user.dir") "test-resources" "another-project"))
-                               []
-                               false)))
-      "Can run a once-problematic project"))
+  (is (any? (sut/impl "clojure"
+                      "deps.edn"
+                      (str (io/file (System/getProperty "user.dir") "test-resources" "another-project"))
+                      []
+                      false))
+      "Can run a once-problematic project")
+
+  (let [v (sut/impl "clojure"
+                    "deps.edn"
+                    (System/getProperty "user.dir")
+                    ["-Sdeps"
+                     (pr-str '{:deps {nrepl/nrepl {:mvn/version "1.0.0"} cider/cider-nrepl {:mvn/version "0.36.0"} refactor-nrepl/refactor-nrepl {:mvn/version "3.9.0"}} :aliases {:cider/nrepl {:main-opts ["-m" "nrepl.cmdline" "--middleware" "[refactor-nrepl.middleware/wrap-refactor,cider.nrepl/cider-middleware]"]}}})
+                     "-M:dev:test:cider/nrepl"]
+                    false)]
+    (testing v
+      (is (string/starts-with? v
+                               "clojure -Sforce -Srepro -J-XX:-OmitStackTraceInFastThrow -J-Dclojure.main.report=stderr -Scp")
+          "Looks like a well-formed `clojure` invocation")
+      (is (string/ends-with? v
+                             "-m nrepl.cmdline --middleware [refactor-nrepl.middleware/wrap-refactor,cider.nrepl/cider-middleware]")
+          "Adds cider-nrepl's --main program based on the -Sdeps and -M args")
+      (is (string/includes? v
+                            ".m2/repository/cider/cider-nrepl/0.36.0/cider-nrepl-0.36.0.jar")
+          "Adds the cider-nrepl dep and program based on the -Sdeps and -M args"))))
